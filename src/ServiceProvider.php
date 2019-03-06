4bc1ae0f2f36c69b78403fe6c5618c99235fb3ec
Austin Passy <367897+thefrosty@users.noreply.github.com>
2019-03-06 09:29:27 -0800

    Cleanup the Service Provider.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,15 +10,14 @@ use TheFrosty\WpUpgradeTaskRunner\Tasks\TaskLoader;
 
 /**
  * Class ServiceProvider
- *
- * @package OpenFit
+ * @package TheFrosty\WpUpgradeTaskRunner
  */
 class ServiceProvider implements ServiceProviderInterface
 {
     public const HTTP_FOUNDATION_REQUEST = 'http.request';
     public const TASK_LOADER = 'upgrade.task_loader';
     public const UPGRADE_MODEL_FACTORY = 'upgrade.model_factory';
-    public const UPGRADES_LIST_TABLE = 'upgrades.list_table';
+    public const UPGRADE_PROVIDER = 'upgrade.provider';
 
     /**
      * Register services.
@@ -26,23 +25,20 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register(PimpleContainer $container): void
     {
-        $container[self::HTTP_FOUNDATION_REQUEST] = function () { // phpcs:ignore
+        $container[self::HTTP_FOUNDATION_REQUEST] = static function () { // phpcs:ignore
             return Request::createFromGlobals();
         };
 
-        $container[self::UPGRADE_MODEL_FACTORY] = function (): UpgradeModelFactory {
+        $container[self::UPGRADE_MODEL_FACTORY] = static function (): UpgradeModelFactory {
             return new UpgradeModelFactory();
         };
 
-        $container[self::TASK_LOADER] = function (PimpleContainer $container): TaskLoader {
+        $container[self::TASK_LOADER] = static function (PimpleContainer $container): TaskLoader {
             return new TaskLoader($container[self::UPGRADE_MODEL_FACTORY]);
         };
 
-        $container[self::UPGRADES_LIST_TABLE] = function (PimpleContainer $container): UpgradesListTable {
-            if (!\class_exists('WP_List_Table')) {
-                require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
-            }
-            return new UpgradesListTable($container);
+        $container[self::UPGRADE_PROVIDER] = static function (PimpleContainer $container): Upgrade {
+            return new Upgrade($container);
         };
     }
 }
