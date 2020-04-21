c8c9dfc46f2eccf15a404b40b033ccd2609089ca
Austin Passy <367897+thefrosty@users.noreply.github.com>
2020-04-21 10:16:40 -0700

    Update Symfony HTTP Foundation

--- upgrade-task-runner.php
+++ upgrade-task-runner.php
@@ -6,7 +6,7 @@
  * Plugin Name: Upgrade Task Runner
  * Plugin URI: https://github.com/thefrosty/wp-upgrade-task-runner
  * Description: A WordPress plugin for developers to write custom migration tasks.
- * Version: 2.1.1
+ * Version: 2.1.2
  * Author: Austin Passy
  * Author URI: https://github.com/thefrosty
  * Requires at least: 5.1
@@ -19,7 +19,7 @@
 namespace TheFrosty\WpUpgradeTaskRunner;
 
 const SLUG = 'wp-upgrade-task-runner';
-const VERSION = '2.1.1';
+const VERSION = '2.1.2';
 
 use TheFrosty\WpUpgradeTaskRunner\Cli\DispatchTasks;
 use TheFrosty\WpUpgradeTaskRunner\Upgrade\DbUpgrade;
