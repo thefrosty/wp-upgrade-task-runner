ed53e286beb4057fea46e6c4b5aa20feacabbea6
Austin Passy <367897+thefrosty@users.noreply.github.com>
2021-11-01 11:35:59 -0700

    Fix: Implicit array creation is disallowed.

--- src/Tasks/TaskLoader.php
+++ src/Tasks/TaskLoader.php
@@ -169,6 +169,7 @@ class TaskLoader implements \IteratorAggregate, WpHooksInterface
             return [];
         }
 
+        $tasks = [];
         foreach ($this as $task) {
             if (!($task instanceof TaskRunnerInterface)) {
                 continue;
@@ -177,7 +178,7 @@ class TaskLoader implements \IteratorAggregate, WpHooksInterface
             $tasks[] = $task;
         }
 
-        return $tasks ?? [];
+        return $tasks;
     }
 
     /**
